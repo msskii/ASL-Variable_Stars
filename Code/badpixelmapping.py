3ac26f2369cc0fea934ece1b73b9f43c96f3af47
grungger <123024860+grungger@users.noreply.github.com>
2023-01-18 23:05:55 +0100

    badpixelmapping fn added

---
+++ Code/badpixelmapping.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 18 22:51:14 2023
+
+@author: Gian
+"""
+import numpy as np
+
+def badpixelmapping(flat_data, dark_data):
+    '''takes flat data (and dark data) from ccd and identifies outliers
+    (5 sigma) that should be excluded in the science frames'''
+    masterflat = np.median(flat_data-dark_data, axis=0)
+    flatavg = np.sum(masterflat/masterflat.size)
+    flatsig = np.sqrt(np.sum((masterflat-np.sum(masterflat)/masterflat.size)**2)/masterflat.size)
+    badpixelmap = masterflat < flatavg-5*flatsig
+    return badpixelmap
+
+
