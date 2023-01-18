50d982c9136b60e1cad874e959b37be229eb00eb
grungger <123024860+grungger@users.noreply.github.com>
2023-01-18 22:45:50 +0100

    masterdark file

---
+++ Code/masterdark.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 18 22:39:02 2023
+
+@author: Gian
+"""
+import numpy as np
+from badpixelinterpolation.py import badpixelinterpolation
+
+def masterdark(data):
+    '''given a numpy array of dark frames the masterdark is returned'''
+    mstdark = np.median(data, axis=0)
+    return mstdark
