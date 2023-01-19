f3ee2e8c495444e2ec655dd6749a0a284bd030bf
grungger <123024860+grungger@users.noreply.github.com>
2023-01-19 23:51:21 +0100

    garbage

--- Code/maincode.py
+++ Code/maincode.py
@@ -48,12 +48,12 @@ print("done Masterflat", time.time() - start)
 # dark subtraction, flat division and badpixel removal
 from badpixelinterpolation import badpixelinterpolation
 from darkflatsubtraction import darkflatsubtraction
-# good_TVLyn_4s = np.zeros(data[4].shape)
-# good_TVLyn_10s = np.zeros(data[5].shape)
-# for i in np.arange(data[4][:,0,0].size):
-#     good_TVLyn_4s[i] = badpixelinterpolation(data[4][i],badpixelmap)
-# for i in np.arange(data[5][:,0,0].size):
-#     good_TVLyn_10s[i] = badpixelinterpolation(data[5][i],badpixelmap)
+good_TVLyn_4s = np.zeros(data[4].shape)
+good_TVLyn_10s = np.zeros(data[5].shape)
+for i in np.arange(data[4][:,0,0].size):
+    good_TVLyn_4s[i] = badpixelinterpolation(data[4][i],badpixelmap)
+for i in np.arange(data[5][:,0,0].size):
+    good_TVLyn_10s[i] = badpixelinterpolation(data[5][i],badpixelmap)
 ####### badpixel to be fixed
 ### change TVLyn data names to good_TVLyn after bad pixel has been done in the darkflatsubtraction section (replace data 4 and data 5!
 
