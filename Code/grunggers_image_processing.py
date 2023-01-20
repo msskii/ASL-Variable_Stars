acd15db7cb63e0ee426c501f37172f02c65a00f5
Sirkii <bigsplitt@gmail.com>
2023-01-20 18:27:34 +0100

    Update grunggers_image_processing.py

--- Code/grunggers_image_processing.py
+++ Code/grunggers_image_processing.py
@@ -1,9 +1,8 @@
 import numpy as np
 from skimage import draw
 
-
-def peak_finder(data, startx, starty, threshold, box_radius=5, abort=500):
-    highestsum = -100
+#
+def peak_finder(data, startx, starty, threshold, box_radius=10, abort=500):
     for r in range(abort):
         rx, ry = draw.circle_perimeter(startx, starty, radius=r+1, shape=data.shape)
         for xo,yo in zip(rx,ry):
@@ -11,32 +10,12 @@ def peak_finder(data, startx, starty, threshold, box_radius=5, abort=500):
             mean = np.median(data[mx,my])
             if(mean >= threshold):
                 return (xo, yo)
-            if(mean > highestsum):
-                highestsum = mean
-        #print(r, highestsum)
     return (0,0)
 
 
-def peak_finder_it(data, startx, starty, threshold=50, box_radius=5, abort=500):
+def peak_finder_it(data, startx, starty, threshold=50, box_radius=10, abort=500):
     pos = np.array(len(data[:,0,0]), dtype=object)
     for i in range(len(data[:,0,0])):
-        pos[i] = peak_finder(data[i], startx, starty, threshold=50, box_radius=5, abort=500)
+        pos[i] = peak_finder(data[i], startx, starty, threshold, box_radius=box_radius, abort=abort)
         (startx, starty) = pos[i]
     return pos
-
-
-from astropy.io import fits
-import matplotlib.pyplot as plt
-
-data = fits.getdata("/Users/Keanu/Documents/GitHub/ASL-Variable_Stars/data/04 - Edited Images/01 - TV Lyn/10s/Cleaned/mes_2023-01-18_02_56_56.FITS",ext=0)
-#plt.plot(data, "test")
-data = np.array(data, dtype=np.float64) - float(np.median(data))
-print(data.shape)
-peak = peak_finder(data,1400,2600, 50)
-print(peak)
-
-
-plt.imshow(data, cmap='gray', vmin=0, vmax=300)
-plt.scatter(peak[1], peak[0])
-plt.title("title")
-plt.show()
