08ab8f5161532a533db66018746342fbad66b86d
grungger <123024860+grungger@users.noreply.github.com>
2023-01-20 17:15:29 +0100

    y axis inverted

--- Code/plotter.py
+++ Code/plotter.py
@@ -5,6 +5,7 @@ import numpy as np
 def plot(data, title, vmax=200):
     data = np.array(data, dtype=np.float64) - float(np.median(data))
     plt.imshow(data, cmap='gray', vmin=0, vmax=vmax)
+    plt.gca().invert_yaxis()
     plt.colorbar()
     plt.title(title)
     plt.show()
